49d7360c573c053cd30eca0c9251459982acef96
vsxingchen <37962879@qq.com>
2018-09-10 16:10:51 +0800

    初始化

--- boot-backend/src/main/java/com/admin/server/ServerApplication.java
+++ boot-backend/src/main/java/com/admin/server/ServerApplication.java
@@ -2,6 +2,13 @@ package com.admin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.WebApplicationInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 启动类
@@ -10,11 +17,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  *         2017年4月18日
  */
+
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+
+	/*protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+
+		return application.sources(ServerApplication.class);
+	}*/
 }
